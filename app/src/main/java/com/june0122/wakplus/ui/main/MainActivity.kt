714c000234ddf08b92b8b353881488155d20f1a0
june0122 <june0122@naver.com>
2022-04-02 20:22:55 +0900

    initial commit

---
+++ app/src/main/java/com/june0122/wakplus/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.june0122.wakplus.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.june0122.wakplus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
