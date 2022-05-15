e293c7bf10e8d918382f4f98390c1992c901f539
june0122 <june0122@naver.com>
2022-05-15 20:35:36 +0900

    #22 refactor: 각 SNS의 API 작업을 별도의 Repository로 분리

---
+++ app/src/main/java/com/june0122/wakplus/data/repository/YoutubeRepository.kt
@@ -0,0 +1,93 @@
+package com.june0122.wakplus.data.repository
+
+import com.june0122.wakplus.data.api.YoutubeService
+import com.june0122.wakplus.data.entity.Content
+import com.june0122.wakplus.data.entity.ContentInfo
+import com.june0122.wakplus.data.entity.IdSet
+import com.june0122.wakplus.data.entity.YoutubeVideoInfo
+import com.june0122.wakplus.utils.SNS
+import javax.inject.Inject
+
+class YoutubeRepository @Inject constructor(
+    private val youtubeApi: YoutubeService,
+) {
+    suspend fun getYoutubeVideos(idSet: IdSet, profileUrl: String): List<Content> {
+        return youtubeApi.run {
+            getChannelVideos(channelId = idSet.youtubeId, order = ORDER_BY_DATE)
+                .items
+                .map { video ->
+                    getVideoInfo(id = video.id.videoId)
+                        .items[0]
+                        .let { videoInfo ->
+                            getYoutubeContent(videoInfo, profileUrl)
+                        }
+                }
+        }
+    }
+
+    // 할당량 최적화를 위해 플레이리스트를 통한 채널 비디오 검색
+    private suspend fun getYoutubeVideosByPlaylist(idSet: IdSet, profileUrl: String): List<Content> {
+        return youtubeApi.run {
+            getPlaylists(channelId = idSet.youtubeId)
+                .items
+                .flatMap { playlist ->
+                    getPlaylistItems(id = playlist.id)
+                        .items
+                        .filter { playlistItem ->
+                            playlistItem.status.privacyStatus != PRIVACY_PRIVATE
+                        }.map { publicItem ->
+                            getVideoInfo(id = publicItem.snippet.resourceId.videoId)
+                                .items[0]
+                                .let { videoInfo ->
+                                    getYoutubeContent(videoInfo, profileUrl)
+                                }
+                        }
+                }
+        }
+    }
+
+    private fun getYoutubeContent(videoInfo: YoutubeVideoInfo, profileUrl: String) =
+        Content(
+            contentId = videoInfo.id,
+            contentType = SNS.YOUTUBE,
+            contentInfo = ContentInfo(
+                videoInfo.id,
+                "streamId",
+                videoInfo.snippet.channelId,
+                "userLogin",
+                videoInfo.snippet.channelTitle,
+                videoInfo.snippet.title,
+                videoInfo.snippet.description,
+                "createdAt",
+                videoInfo.snippet.publishedAt,
+                PREFIX_YOUTUBE_VIDEO_URL + videoInfo.id,
+                PREFIX_YOUTUBE_CHANNEL_URL + videoInfo.snippet.channelId,
+                videoInfo.snippet.thumbnails.high.url,
+                "viewable",
+                videoInfo.statistics.viewCount,
+                videoInfo.snippet.defaultAudioLanguage,
+                videoInfo.kind,
+                videoInfo.contentDetails.duration,
+                "mutedSegments",
+                "displayName",
+                "profileImageUrl"
+            ),
+            profileUrl = profileUrl,
+            isFavorite = false
+        )
+
+    companion object {
+        private const val PREFIX_YOUTUBE_VIDEO_URL = "https://youtu.be/"
+        private const val PREFIX_YOUTUBE_CHANNEL_URL = "https://www.youtube.com/channel/"
+
+        private const val ORDER_BY_DATE = "date"
+        private const val ORDER_BY_RATING = "rating"
+        private const val ORDER_BY_RELEVANCE = "relevance"
+        private const val ORDER_BY_TITLE = "title"
+        private const val ORDER_BY_VIDEO_COUNT = "videoCount"
+        private const val ORDER_BY_VIEW_COUNT = "viewCount"
+
+        private const val PRIVACY_PUBLIC = "public"
+        private const val PRIVACY_PRIVATE = "private"
+    }
+}
