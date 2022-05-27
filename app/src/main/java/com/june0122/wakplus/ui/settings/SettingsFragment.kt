852f2c65912c0748efd9365901260dcd69bc75fe
june0122 <june0122@naver.com>
2022-05-27 20:47:03 +0900

    #31 feat: 설정 화면의 기본 레이아웃 및 코드 정의

---
+++ app/src/main/java/com/june0122/wakplus/ui/settings/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package com.june0122.wakplus.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.june0122.wakplus.R
+import com.june0122.wakplus.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
