2cb8a6c5ead2981e4b08f7a70df68f010eff77b3
june0122 <june0122@naver.com>
2022-06-16 21:05:55 +0900

    #31 feat: 테마 색상 추가

--- app/src/main/java/com/june0122/wakplus/ui/settings/theme/ThemeListAdapter.kt
+++ app/src/main/java/com/june0122/wakplus/ui/settings/theme/ThemeListAdapter.kt
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.june0122.wakplus.R
 import com.june0122.wakplus.data.entity.Theme
 import com.june0122.wakplus.databinding.ItemThemeBinding
+import com.june0122.wakplus.utils.ISEDOL.GOSEGU
 import com.june0122.wakplus.utils.ISEDOL.INE
 import com.june0122.wakplus.utils.ISEDOL.JINGBURGER
+import com.june0122.wakplus.utils.ISEDOL.JURURU
+import com.june0122.wakplus.utils.ISEDOL.LILPA
 import com.june0122.wakplus.utils.ISEDOL.VIICHAN
 import com.june0122.wakplus.utils.listeners.ThemeClickListener
 
@@ -15,9 +18,12 @@ class ThemeListAdapter(
     private val themeClickListener: ThemeClickListener
 ) : RecyclerView.Adapter<ThemeViewHolder>() {
     private val themeList = mutableListOf(
-        Theme(INE, R.color.InePrimary to R.color.InePrimaryDark),
-        Theme(JINGBURGER, R.color.JingburgerPrimary to R.color.JingburgerPrimaryDark),
-        Theme(VIICHAN, R.color.ViichanPrimary to R.color.ViichanPrimaryDark),
+        Theme(INE, R.color.InePrimary to R.color.ine_secondary),
+        Theme(JINGBURGER, R.color.JingburgerPrimary to R.color.JingburgerSecondary),
+        Theme(LILPA, R.color.LilpaPrimary to R.color.LilpaSecondary),
+        Theme(JURURU, R.color.JururuPrimary to R.color.JururuSecondary),
+        Theme(GOSEGU, R.color.GoseguPrimary to R.color.GoseguSecondary),
+        Theme(VIICHAN, R.color.viichan_primary to R.color.ViichanSecondary),
     )
 
     override fun getItemCount(): Int = themeList.size
--- app/src/main/java/com/june0122/wakplus/utils/ThemeManager.kt
+++ app/src/main/java/com/june0122/wakplus/utils/ThemeManager.kt
@@ -2,8 +2,11 @@ package com.june0122.wakplus.utils
 
 import com.june0122.wakplus.R
 import com.june0122.wakplus.data.repository.impl.PreferencesRepositoryImpl
+import com.june0122.wakplus.utils.ISEDOL.GOSEGU
 import com.june0122.wakplus.utils.ISEDOL.INE
 import com.june0122.wakplus.utils.ISEDOL.JINGBURGER
+import com.june0122.wakplus.utils.ISEDOL.JURURU
+import com.june0122.wakplus.utils.ISEDOL.LILPA
 import com.june0122.wakplus.utils.ISEDOL.VIICHAN
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -18,8 +21,11 @@ class ThemeManager @Inject constructor(
     fun saveTheme(activeTheme: String) {
         val themeResourseId = when (activeTheme) {
             INE -> R.style.Theme_Ine
-            VIICHAN -> R.style.Theme_Viichan
             JINGBURGER -> R.style.Theme_Jingburger
+            LILPA -> R.style.Theme_Lilpa
+            JURURU -> R.style.Theme_Jururu
+            GOSEGU -> R.style.Theme_Gosegu
+            VIICHAN -> R.style.Theme_Viichan
             else -> R.style.Theme_Ine
         }
 
