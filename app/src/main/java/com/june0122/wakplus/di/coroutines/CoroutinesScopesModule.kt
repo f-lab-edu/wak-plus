74cb6a570ad834d7365aedcde0f220209e91ff1c
june0122 <june0122@naver.com>
2022-08-16 22:55:32 +0900

    #43 refactor: Coroutines Scope 및 Dispatcher 모듈 생성

---
+++ app/src/main/java/com/june0122/wakplus/di/coroutines/CoroutinesScopesModule.kt
@@ -0,0 +1,27 @@
+package com.june0122.wakplus.di.coroutines
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesScopesModule {
+
+    @Singleton
+    @ApplicationScope
+    @Provides
+    fun providesCoroutineScope(
+        @DefaultDispatcher defaultDispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
+}
